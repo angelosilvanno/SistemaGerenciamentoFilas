1ae5e8a5191188f45b1e44603567023ffbf97a62
Savio23br <savio.oliveira.jeronimo2005@gmail.com>
2026-05-28 19:30:18 -0300

    feat: adiciona funções utilitárias em módulos de autenticação e usuário

--- src/controllers/FilaController.java
+++ src/controllers/FilaController.java
@@ -1,5 +1,15 @@
 package controllers;
 
 public class FilaController {
-    
+    public void processarEntradaFila(){
+        
+   
+    }
+    public void processarChamadaSenha(){
+        
+    }
+    public void  validarAcesso(){
+        
+    }
+
 }
--- src/views/SistemaView.java
+++ src/views/SistemaView.java
@@ -1,5 +1,13 @@
 package views;
 
 public class SistemaView {
-    
+    public void exibirMenuCidadao(){
+
+    }
+    public void exibirPainelAtendente(){
+        
+    }
+    public void exibirRelatorios(){
+        
+    }
 }
