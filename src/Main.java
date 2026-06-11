23d1f006114c84b2cef56ce70f4c7a49fc07a464
Ângelo <familiasabino14@gmail.com>
2026-06-11 18:57:23 -0300

    feat: adiciona inserção de usuários no banco de dados

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,27 @@
 import dao.DadosRepository;
 import views.SistemaView;
+import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args){
         DadosRepository repositorio = new DadosRepository();
         repositorio.conectarBanco();
+
+        try {
+            Statement stmt = repositorio.getConexao().createStatement();
+            
+            stmt.execute("INSERT OR IGNORE INTO usuarios (cpf, nome, senha, tipo, ativo) " +
+            "VALUES ('11111111111', 'Ana Gestora', 'admin123', 'Administrador', 1);");
+
+            stmt.execute("INSERT OR IGNORE INTO usuarios (cpf, nome, senha, tipo, ativo) " +
+            "VALUES ('22222222222', 'Dr. Henrique', 'atend123', 'Atendente', 1);");
+
+            stmt.execute("INSERT OR IGNORE INTO usuarios (cpf, nome, senha, tipo, ativo) " +
+            "VALUES ('33333333333', 'Ângelo Silvano', 'cidadao123', 'Cidadao', 1);");
+
+        } catch (Exception e) {
+    
+        }
 
         SistemaView tela = new SistemaView();
         tela.exibirMenuPrincipal();
